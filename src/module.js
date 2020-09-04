48413b835e996b2b4a7433afe045aa268b889e82
gradiusalisik <gradiusalisik@gmail.com>
2020-09-05 02:21:59 +0300

    finish css-layout

--- src/module.js
+++ src/module.js
@@ -1,7 +1,7 @@
-console.log("module.js");
+console.log('module.js');
 
 async function start() {
-  await Promise.resolve("async working");
+  await Promise.resolve('async working');
 }
 
 start().then(console.log);
