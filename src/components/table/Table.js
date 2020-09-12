fe0ff11fbed838651efbee84f6393ca4243fde11
Vladislav Grigorzhevskii <gradiusalisik@users.noreply.github.com>
2020-09-12 03:02:03 +0300

    Feature/resize (#4)
    * for memory my decided old
    
    * finish table resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,9 +1,23 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from './table.template';
+import {resizeHandler} from './table.resize';
+import {shouldResize} from './table.functions';
 
 export class Table extends ExcelComponent {
   static className = 'table';
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown'],
+    })
+  }
+
   toHTML() {
     return createTable(20);
+  }
+
+  onMousedown(event) {
+    if (shouldResize) {
+      resizeHandler(this.$root, event);
+    }
   }
 }
